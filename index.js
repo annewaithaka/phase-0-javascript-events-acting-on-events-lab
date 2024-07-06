31a7ce2a6d30f76a7e91c1e65d04db0fea11dcaa
annewaithaka <annewaithaka68@gmail.com>
2024-07-06 22:28:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function moveDodgerLeft() {
+    let dodger = document.getElementById('dodger');
+    let leftPosition = parseInt(dodger.style.left) || 0; 
+    let newPosition = leftPosition - 1; 
+    if (newPosition >= 0) {
+      dodger.style.left = `${newPosition}px`;
+    } else{
+        
+    }
+  }
+
+  function moveDodgerRight() {
+    let dodger = document.getElementById('dodger');
+    let leftPosition = parseInt(dodger.style.left) || 0;
+    let newPosition = leftPosition + 1; 
+    if (newPosition <= 360) {
+      dodger.style.left = `${newPosition}px`; 
+    }
+}
